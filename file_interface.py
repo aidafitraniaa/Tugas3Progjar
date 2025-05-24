cb77b69de11536e3db7d23d207af2fe1a0a0398d
Aida Fitrania <91545802+aidafitraniaa@users.noreply.github.com>
2025-05-24 17:59:28 +0700

    Add

---
+++ file_interface.py
@@ -0,0 +1,51 @@
+import os
+import json
+import base64
+from glob import glob
+import shutil
+
+class FileInterface:
+    def __init__(self):
+        os.chdir('files/')
+
+    def list(self,params=[]):
+        try:
+            filelist = glob('*.*')
+            return dict(status='OK',data=filelist)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+    def get(self,params=[]):
+        try:
+            filename = params[0]
+            if (filename == ''):
+                return None
+            fp = open(f"{filename}",'rb')
+            isifile = base64.b64encode(fp.read()).decode()
+            return dict(status='OK',data_namafile=filename,data_file=isifile)
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+    
+    def upload(self, params=[]):
+        try:
+            filename = params[0]
+            file_content = base64.b64decode(params[1])
+            fp = open(filename, 'wb+')
+            fp.write(file_content)
+            fp.close()
+            return dict(status='OK')
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+    
+    def delete(self, params=[]):
+        try:
+            filename = params[0]
+            os.remove(filename)
+            return dict(status='OK')
+        except Exception as e:
+            return dict(status='ERROR',data=str(e))
+
+if __name__=='__main__':
+    f = FileInterface()
+    print(f.list())
+    print(f.get(['pokijan.jpg']))
